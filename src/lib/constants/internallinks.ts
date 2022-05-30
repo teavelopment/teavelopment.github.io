04a604bfe73d71583f959ebffbfbd2085c55d28c
Jlnrrg <julian.roerig.k@gmail.com>
2022-05-30 14:50:13 +0200

    bugfix - base home link

--- src/lib/constants/internallinks.ts
+++ src/lib/constants/internallinks.ts
@@ -1,7 +1,7 @@
 import { base } from '$app/paths';
 
 export default abstract class InternalLinks {
-	static home = base;
+	static home = base + '/';
 	static apps = base + '/apps';
 	static countDoko = base + '/apps/countDoko';
 	static openSource = base + '/openSource';
--- src/lib/sections/footer/Teavelopment.svelte
+++ src/lib/sections/footer/Teavelopment.svelte
@@ -2,15 +2,18 @@
 	import AppleStore from '$lib/components/labels/appleStore.svelte';
 	import PlayStore from '$lib/components/labels/playStore.svelte';
 	import ExternalLinks from '$lib/constants/externallinks';
+	import InternalLinks from '$lib/constants/internallinks';
 	import SourcesLinks from '$lib/constants/sourceslinks';
 </script>
 
 <div>
-	<img
-		class="object-scale-down max-w-xs shrink"
-		src={SourcesLinks.logoTeavelopment}
-		alt="Teavelopment Poster"
-	/>
+	<a href={InternalLinks.home}>
+		<img
+			class="object-scale-down max-w-xs shrink"
+			src={SourcesLinks.logoTeavelopment}
+			alt="Teavelopment Poster"
+		/>
+	</a>
 	<div class="w-full flex flex-row justify-center gap-x-4">
 		<PlayStore url={ExternalLinks.playStoreTeavelopment} />
 		<AppleStore url="" />
